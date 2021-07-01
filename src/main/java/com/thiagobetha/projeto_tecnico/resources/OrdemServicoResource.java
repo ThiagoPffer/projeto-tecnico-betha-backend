f4a63cb2039c911efce92709305f7a3c2a7e45bb
Thiago Piffer <thiago.ramos@betha.com.br>
2021-07-01 10:49:17 -0300

    Resource e Service da OrdemServico

--- src/main/java/com/thiagobetha/projeto_tecnico/resources/OrdemServicoResource.java
+++ src/main/java/com/thiagobetha/projeto_tecnico/resources/OrdemServicoResource.java
@@ -1,43 +1,34 @@
 package com.thiagobetha.projeto_tecnico.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.thiagobetha.projeto_tecnico.domain.OrdemServico;
-import com.thiagobetha.projeto_tecnico.domain.enums.SituacaoOrdemServico;
+import com.thiagobetha.projeto_tecnico.services.OrdemServicoService;
 
 @RestController
 @RequestMapping(value="/ordensservico")
 public class OrdemServicoResource {
 	
-	/*@Autowired
-	private OrdemServicoService service;*/
+	@Autowired
+	private OrdemServicoService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public List<OrdemServico> listAll() {
-		
-		List<OrdemServico> lista = new ArrayList<>();
-		
-		String item1 = "Televisor";
-		String item2 = "Ferro de passar";
-		
-		OrdemServico os1 = new OrdemServico(SituacaoOrdemServico.EM_ANALISE, "PENDENTE", "Maria Silva");
-		os1.setItens(item1);
-
-		OrdemServico os2 = new OrdemServico(SituacaoOrdemServico.EM_ANALISE, "PENDENTE", "Pedro Silva");
-		os2.setItens(item2);
-		
-		lista.add(os1);
-		lista.add(os2);
-		
-		return lista;
-		
-		/*List<OrdemServico> list = service.findAll();
-		return ResponseEntity.ok().body(list);*/
+	public ResponseEntity<List<OrdemServico>> listAll() {	
+		List<OrdemServico> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<OrdemServico> listOne(@PathVariable Integer id) {
+		OrdemServico obj = service.findOne(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
--- src/main/java/com/thiagobetha/projeto_tecnico/services/OrdemServicoService.java
+++ src/main/java/com/thiagobetha/projeto_tecnico/services/OrdemServicoService.java
@@ -1,6 +1,7 @@
 package com.thiagobetha.projeto_tecnico.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,11 @@ public class OrdemServicoService {
 	
 	public List<OrdemServico> findAll(){
 		return repo.findAll();
+	}
+	
+	public OrdemServico findOne(Integer id) {
+		Optional<OrdemServico> obj = repo.findById(id);
+		return obj.orElse(null);
 	}
 	
 }
